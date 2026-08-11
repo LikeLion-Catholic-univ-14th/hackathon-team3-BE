d94c164f3026fbb4efacdd320123d494c3447112
githeobu <heominji1012@gmail.com>
2026-08-11 12:19:11 +0900

    initial commit

---
+++ src/main/java/com/example/hackathon_team3_be/HackathonTeam3BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackathon_team3_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonTeam3BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonTeam3BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hackathon_team3_be/HackathonTeam3BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackathon_team3_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonTeam3BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
